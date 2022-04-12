215a1c0bdfb3bc98ef26de57b44c1190718887e2
kijung <daiywlf5251@gmail.com>
2022-04-12 17:40:40 +0900

    add Domain Entity without Category

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") // url
     public String hello(Model model) { // Model로 뷰에 데이터를 넘긴다.
         model.addAttribute("data", "hello!!!");
         return "hello"; //화면 이름
