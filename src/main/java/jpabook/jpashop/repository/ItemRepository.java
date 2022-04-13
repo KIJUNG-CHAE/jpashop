6f0abbda8f0e32b21e5ef61dee3f3219862d8a41
kijung <daiywlf5251@gmail.com>
2022-04-13 21:48:27 +0900

    add Item Repository, Item Service

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { //아이템을 새로 만드는 경우 디비에 넣기전에 아이디가 없다.
+            em.persist(item); // item을 추가.
+        } else {
+            em.merge(item); // item을 업데이트한다고 보면 된다.
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
